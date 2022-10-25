41b9e584e5017af9166af38d824ceb6b8b57a09c
Raditya Firman Syaputra <radit@student.uns.ac.id>
2022-10-25 21:09:20 +0700

    Enhance : Adding more algorithm Encrypt & Decrypt

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,21 @@ window.onload = function () {
   const decryptText = document.getElementsByClassName("decrypt-text");
   const encryptText = document.getElementsByClassName("encrypt-text");
 
+
+  function debounce(func, timeout = 500) {
+    let timer;
+    return (...args) => {
+      clearTimeout(timer);
+      timer = setTimeout(() => { func.apply(this, args); }, timeout);
+    };
+  }
+
+
   const translatingText = async (text, action) => {
     const selectedAlgorithm =
       document.getElementsByClassName("algoritma")[0].value;
 
-    const result = await fetch("/algorithm.php", {
+    const result = await fetch("./algorithm.php", {
       method: "POST",
       body: JSON.stringify({
         text: text,
@@ -16,22 +26,45 @@ window.onload = function () {
       }),
     }).then((res) => res.json());
 
+    if (result.error) {
+      Toastify({
+        text: result?.error,
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        backgroundColor: "red",
+        stopOnFocus: true,
+      }).showToast();
+
+      return false
+    }
     return result?.result;
   };
 
-  document.addEventListener("translateText", async (event) => {
-    const { action, text } = event.detail;
+  document.addEventListener("translateText", debounce(async (e) => {
+    const { action, text } = e.detail;
+    const result = await translatingText(text, action);
+    if (result) {
+      Toastify({
+        text: "Berhasil",
+        duration: 3000,
+        newWindow: true,
+        close: true,
+        gravity: "top",
+        position: "right",
+        backgroundColor: "green",
+        stopOnFocus: true,
+      }).showToast();
 
-    await translatingText(text, action).then((result) => {
-      if(!!result){
-        if (action === "encrypt") {
-          encryptText[0].value = result;
-        } else {
-          decryptText[0].value = result;
-        }
+      if (action === "encrypt") {
+        encryptText[0].value = result;
+      } else {
+        decryptText[0].value = result;
       }
-    });
-  });
+    }
+  }, 500));
 
   selectedAlgorithm[0].addEventListener("change", function () {
     const event = new CustomEvent("translateText", {
--- vendor/composer/autoload_psr4.php
+++ vendor/composer/autoload_psr4.php
@@ -6,5 +6,6 @@ $vendorDir = dirname(__DIR__);
 $baseDir = dirname($vendorDir);
 
 return array(
-    'PhpAes\\' => array($vendorDir . '/phpaes/phpaes/src'),
+    'MiladRahimi\\PhpCrypt\\Tests\\' => array($vendorDir . '/miladrahimi/phpcrypt/tests'),
+    'MiladRahimi\\PhpCrypt\\' => array($vendorDir . '/miladrahimi/phpcrypt/src'),
 );
